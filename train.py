7364db4f7316946ce506fa92649e20c967e3588b
Dewaszmi <maciej.zyczynski@protonmail.com>
2026-03-23 22:27:35 +0100

    cluster setup

--- train.py
+++ train.py
@@ -1,33 +1,26 @@
 import torch
 from datasets import Audio, load_dataset
-from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
-from transformers import (
-    AutoProcessor,
-    BitsAndBytesConfig,
-    MusicgenForConditionalGeneration,
-    Trainer,
-    TrainingArguments,
-)
+from peft import LoraConfig, get_peft_model
+from transformers import (AutoProcessor, BitsAndBytesConfig,
+                          MusicgenForConditionalGeneration, Trainer,
+                          TrainingArguments)
 
-# quantification for a 8GB VRAM GPU (probably not required for newer cards)
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16,
-    bnb_4bit_use_double_quant=True,
-)
+# # quantification for a 8GB VRAM GPU (probably not required for newer cards)
+# bnb_config = BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_quant_type="nf4",
+#     bnb_4bit_compute_dtype=torch.float16,
+#     bnb_4bit_use_double_quant=True,
+# )
 
 # loading model
 model_id = "facebook/musicgen-small"
 processor = AutoProcessor.from_pretrained(model_id)
 model = MusicgenForConditionalGeneration.from_pretrained(
-    model_id, quantization_config=bnb_config, device_map={"": 0}
+    model_id, torch_dtype=torch.float16, device_map="auto",
 )
 
 # setup Lora
-model.gradient_checkpointing_enable()
-model = prepare_model_for_kbit_training(model)
-
 config = LoraConfig(
     r=8,
     lora_alpha=16,
@@ -56,21 +49,19 @@ def preprocess_function(examples):
     return inputs
 
 
-# map dataset to smaller batch to save ram
 tokenized_dataset = dataset.map(preprocess_function, batched=True, batch_size=1)
 
 # training
 training_args = TrainingArguments(
-    output_dir="./musicgen-sewerslvt-lora",
-    per_device_train_batch_size=1,  # Keep this at 1 for 8GB
-    gradient_accumulation_steps=8,  # Simulates a batch size of 8
-    warmup_steps=10,
+    output_dir="./musicgen-style-lora",
+    per_device_train_batch_size=2,
+    gradient_accumulation_steps=4,
     max_steps=100,
     learning_rate=2e-4,
-    fp16=True,  # Use half-precision
-    logging_steps=1,
-    optim="adamw_bnb_8bit",  # Use 8-bit optimizer
+    fp16=True,
+    logging_steps=5,
     save_total_limit=2,
+    remove_unused_columns=False,
 )
 
 trainer = Trainer(
